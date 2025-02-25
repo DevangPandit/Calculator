fadc49efd4623cfe5e626a30cf74d0912513db0c
DevangPandit <panditdevang7@gmail.com>
2025-02-24 21:31:33 -0500

    Updated Version

---
+++ script.js
@@ -0,0 +1,16 @@
+let display = document.getElementById("display");
+
+function clearDisplay(){
+    display.value = "";
+}
+function appendToDisplay(value){
+    display.value += value;
+}
+function calculateResult(){
+    try{
+        display.value = eval(display.value)
+    }
+    catch (e){
+        display.value = "Error";
+    }
+}
